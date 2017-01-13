bb7516f37313edd9bc6350e97d6c7b48c4585f3d
wowchan <ckingwowchan@gmail.com>
2017-01-13 10:46:36 +0800

    optimize emacs alias, add sdkman rc file

--- init.sh
+++ init.sh
@@ -4,4 +4,5 @@ source ~/.zshrc.d/git.rc.sh
 source ~/.zshrc.d/gvm.rc.sh
 source ~/.zshrc.d/pyenv.rc.sh
 source ~/.zshrc.d/q1.rc.sh
+source ~/.zshrc.d/sdkman.rc.sh
 source ~/.zshrc.d/startup.rc.sh
---
+++ sdkman.rc.sh
@@ -0,0 +1,2 @@
+export SDKMAN_DIR="/usr/local/sdkman"
+[[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
