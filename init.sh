0fe74ba25bd20497a7c9275b8fe744b18f948f79
wowchan <ckingwowchan@gmail.com>
2017-01-17 13:53:06 +0800

    add nvm.rc.sh

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@ source ~/.zshrc.d/cmd.rc.sh
 source ~/.zshrc.d/emacs.rc.sh
 source ~/.zshrc.d/git.rc.sh
 source ~/.zshrc.d/gvm.rc.sh
+source ~/.zshrc.d/nvm.rc.sh
 source ~/.zshrc.d/pyenv.rc.sh
 source ~/.zshrc.d/q1.rc.sh
 source ~/.zshrc.d/sdkman.rc.sh
