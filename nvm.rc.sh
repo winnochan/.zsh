71aa8aa32dc66ebbc047e680cbef53c7d3573cb1
cwh0594 <cwh0594@175game.com>
2017-02-20 10:48:41 +0800

    add nvm alias

--- nvm.rc.sh
+++ nvm.rc.sh
@@ -1,6 +1,6 @@
 alias npmga="npm i -g angular-cli express-generator grunt-cli jingo js-beautify mean-cli nodemon pug-cli tern vmd"
-
 alias npmlg="npm list -g --depth 0"
+alias nvmin="nvm install $(nvm version-remote --lts=Boron)"
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
