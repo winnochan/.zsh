4d69fb730d3d287461762893d0b21fb3625f5d95
cwh0594 <cwh0594@175game.com>
2017-02-20 15:57:06 +0800

    add py alias

--- pyenv.rc.sh
+++ pyenv.rc.sh
@@ -3,4 +3,5 @@ export PATH="$PATH:$PYENV_ROOT/bin"
 if which pyenv > /dev/null; then
     eval "$(pyenv init -)"
     eval "$(pyenv virtualenv-init -)"
+    alias pydep="pip freeze > requirements.txt"
 fi
