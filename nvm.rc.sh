7d1472ebeb3f2efdd4cb2976465f1f9d401a7192
cwh0594 <cwh0594@175game.com>
2017-02-20 10:51:20 +0800

    modify nvm alias

--- nvm.rc.sh
+++ nvm.rc.sh
@@ -1,4 +1,4 @@
-alias npmga="npm i -g angular-cli express-generator grunt-cli jingo js-beautify mean-cli nodemon pug-cli tern vmd"
+alias npmga="npm i -g @angular/cli express-generator grunt-cli jingo js-beautify mean-cli nodemon pug-cli tern vmd"
 alias npmlg="npm list -g --depth 0"
 alias nvmin="nvm install $(nvm version-remote --lts=Boron)"
 
