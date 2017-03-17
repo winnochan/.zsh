8b252d2641d0fe3309c6c67ababd8f4c84c05590
cwh0594 <cwh0594@175game.com>
2017-03-17 17:37:05 +0800

    add py2 and py3 alias

--- pyenv.rc.sh
+++ pyenv.rc.sh
@@ -4,6 +4,8 @@ if which pyenv > /dev/null; then
     eval "$(pyenv init -)"
     eval "$(pyenv virtualenv-init -)"
     alias pydep="pip freeze > requirements.txt"
+    alias py2="pyenv local $(pyenv versions | grep "  2.7.*$" | tail -1) > /dev/null && python"
+    alias py3="pyenv local $(pyenv versions | grep "  3.5.*$" | tail -1) > /dev/null && python"
 fi
 
 export FLASK_APP="run.py"
