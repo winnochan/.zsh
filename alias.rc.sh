7fb76a5d1f1528e20abb87d9fa1e3857c8bb1974
陈万豪 <wow4chan@gmail.com>
2017-06-21 07:02:20 +0800

    add editor config

--- alias.rc.sh
+++ alias.rc.sh
@@ -2,6 +2,10 @@ alias -s gz="tar -xzvf"
 alias -s tgz="tar -xzvf"
 alias -s zip="unzip"
 alias -s bz2='tar -xjvf'
-alias -s c="emacsclient -t -a emacs"
-alias -s h="emacsclient -t -a emacs"
+alias -s c="emacsclient -t -a vim"
+alias -s h="emacsclient -t -a vim"
+
+export EDITOR="emacsclient -t -a vim"
+# setopt AUTO_LIST
+# setopt AUTO_MENU
 
