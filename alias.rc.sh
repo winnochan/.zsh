df9a6cd2d4b59bd5fdf26cd1c0d78eb441348bdc
陈万豪 <wow4chan@gmail.com>
2017-07-05 14:06:58 +0800

    add alias

--- alias.rc.sh
+++ alias.rc.sh
@@ -5,6 +5,12 @@ alias -s bz2='tar -xjvf'
 alias -s c="emacsclient -t -a vim"
 alias -s h="emacsclient -t -a vim"
 
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias .......='cd ../../../../../..'
+
 export EDITOR="emacsclient -t -a vim"
 # setopt AUTO_LIST
 # setopt AUTO_MENU
