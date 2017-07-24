8c48a81724e1eea653fa49c6e9617ff14b0f1b5f
wowchan <wow4chan@gmail.com>
2017-07-25 00:40:02 +0800

    optimize brew config

--- brew-origin.sh
+++ brew-origin.sh
@@ -2,3 +2,5 @@ cd "$(brew --repo)"
 git remote set-url origin https://github.com/Homebrew/brew.git
 cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
 git remote set-url origin https://github.com/Homebrew/homebrew-core.git
+cd "$(brew --repo)/Library/Taps/caskroom/homebrew-cask"
+git remote set-url origin https://github.com/caskroom/homebrew-cask.git
--- brew-ustc.sh
+++ brew-ustc.sh
@@ -1,4 +1,6 @@
 cd "$(brew --repo)"
 git remote set-url origin https://mirrors.ustc.edu.cn/brew.git
-cd "$(brew --repo)"
-git remote set-url origin https://mirrors.ustc.edu.cn/brew.git
+cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+git remote set-url origin https://mirrors.ustc.edu.cn/homebrew-core.git
+cd "$(brew --repo)/Library/Taps/caskroom/homebrew-cask"
+git remote set-url origin https://mirrors.ustc.edu.cn/homebrew-cask.git
