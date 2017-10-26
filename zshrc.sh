6be8187f3ef3fc485daca1502f274ad3c15120f2
陈万豪 <wow4chan@gmail.com>
2017-10-26 20:05:24 +0800

    modify alias and env

--- zshrc.sh
+++ zshrc.sh
@@ -6,11 +6,29 @@ source $current_dir/antigen.zsh
 antigen use oh-my-zsh
 
 # oh-my-zsh plugins
+antigen bundle autojump
+antigen bundle battery
+antigen bundle brew
+antigen bundle colored-man-pages
+antigen bundle encode64
+antigen bundle extract
 antigen bundle git
 antigen bundle last-working-dir
 antigen bundle per-directory-history
+antigen bundle pyenv
+antigen bundle ssh-agent
+antigen bundle sudo
+antigen bundle svn
+antigen bundle svn-fast-info
+antigen bundle swiftpm
+antigen bundle systemadmin
+antigen bundle tig
+antigen bundle tmux
+antigen bundle virtualenv
+# antigen bundle virtualenvwrapper
 antigen bundle zsh-completion-generator
 antigen bundle zsh_reload
+antigen bundle z
 
 # git repo
 # antigen bundle zdharma/zui
