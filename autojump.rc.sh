cea3c5d131c7425a5529121613eb780060ce0283
wowchan <wow4chan@gmail.com>
2017-11-08 10:10:50 +0800

    optimize config

--- autojump.rc.sh
+++
@@ -1,2 +0,0 @@
-[[ -s /home/wowchan/.autojump/etc/profile.d/autojump.sh ]] && source /home/wowchan/.autojump/etc/profile.d/autojump.sh
-autoload -U compinit && compinit -u
--- brew-tsinghua.sh
+++
@@ -1,4 +0,0 @@
-cd "$(brew --repo)"
-git remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git
-cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
-git remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
