0d2d4113ecd1e7a5e5426a76f6ff3d3c02d6ce93
WinnoChan <winnochan@icloud.com>
2017-11-29 10:33:33 +0800

    update config

--- zshrc.zsh
+++ zshrc.zsh
@@ -2,20 +2,20 @@ export ZSH_HOME=$(cd "$(dirname "$0")"; pwd)
 export ZPLUG_HOME=$ZSH_HOME/zplug
 source $ZPLUG_HOME/init.zsh
 
-zmodload zsh/zprof
+# zmodload zsh/zprof
 
 # zplug "zplug/zplug", hook-build:"zplug --self-manage"
 
 # plugins from prezto
-zplug "sorin-ionescu/prezto", use:"init.zsh", \
-      hook-build:"ln -s $ZPLUG_HOME/repos/sorin-ionescu/prezto ~/.zprezto"
-source $ZSH_HOME/prezto.zsh
+# zplug "sorin-ionescu/prezto", use:"init.zsh", \
+#       hook-build:"ln -s $ZPLUG_HOME/repos/sorin-ionescu/prezto ~/.zprezto"
+# source $ZSH_HOME/prezto.zsh
 
 # plugins from zsh-user
-# zplug "zsh-users/zaw"
+zplug "zsh-users/zaw"
 zplug "zsh-users/zsh-autosuggestions"
-# zplug "zsh-users/zsh-completions"
-# zplug "zsh-users/zsh-history-substring-search"
+zplug "zsh-users/zsh-completions"
+zplug "zsh-users/zsh-history-substring-search"
 # must the end of other plugins
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
@@ -23,7 +23,7 @@ zplug "zsh-users/zsh-syntax-highlighting", defer:2
 # zplug "plugins/autojump", from:oh-my-zsh
 zplug "plugins/colored-man-pages", from:oh-my-zsh
 # zplug "plugins/encode64", from:oh-my-zsh
-zplug "plugins/extract", from:oh-my-zsh
+# zplug "plugins/extract", from:oh-my-zsh
 zplug "plugins/git", from:oh-my-zsh
 zplug "plugins/last-working-dir", from:oh-my-zsh
 zplug "plugins/per-directory-history", from:oh-my-zsh
@@ -56,5 +56,7 @@ if zplug check b4b4r07/enhancd; then
     export ENHANCD_FILTER=fzy
 fi
 
+# custom settings
+set -o emacs
 # custom alias
 source $ZSH_HOME/alias.zsh
