5a6e32ac17810d6deac3c766c8c5a72e604e4736
WinnoChan <winnochan@icloud.com>
2017-11-29 14:22:39 +0800

    update config

--- zshrc.zsh
+++ zshrc.zsh
@@ -9,7 +9,10 @@ source $ZPLUG_HOME/init.zsh
 # plugins from prezto
 # zplug "sorin-ionescu/prezto", use:"init.zsh", \
 #       hook-build:"ln -s $ZPLUG_HOME/repos/sorin-ionescu/prezto ~/.zprezto"
-# source $ZSH_HOME/prezto.zsh
+# zstyle ':prezto:load' pmodule 'editor'
+# if zplug check sorin-ionescu/prezto; then
+#     source $ZSH_HOME/prezto.zsh
+# fi
 
 # plugins from zsh-user
 zplug "zsh-users/zaw"
@@ -19,17 +22,23 @@ zplug "zsh-users/zsh-history-substring-search"
 # must the end of other plugins
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
+zplug "robbyrussell/oh-my-zsh", \
+      hook-build:"ln -s $ZPLUG_HOME/repos/robbyrussell/oh-my-zsh ~/.oh-my-zsh"
+if zplug check robbyrussell/oh-my-zsh; then
+    source $ZSH_HOME/ohmyzsh.zsh
+fi
+
 # plugins from oh-my-zsh
 # zplug "plugins/autojump", from:oh-my-zsh
-zplug "plugins/colored-man-pages", from:oh-my-zsh
+# zplug "plugins/colored-man-pages", from:oh-my-zsh
 # zplug "plugins/encode64", from:oh-my-zsh
 # zplug "plugins/extract", from:oh-my-zsh
-zplug "plugins/git", from:oh-my-zsh
-zplug "plugins/last-working-dir", from:oh-my-zsh
-zplug "plugins/per-directory-history", from:oh-my-zsh
-zplug "plugins/tib", from:oh-my-zsh
+# zplug "plugins/git", from:oh-my-zsh
+# zplug "plugins/last-working-dir", from:oh-my-zsh
+# zplug "plugins/per-directory-history", from:oh-my-zsh
+# zplug "plugins/tib", from:oh-my-zsh
 # zplug "plugins/zsh-completion-generator", from:oh-my-zsh
-zplug "plugins/zsh_reload", from:oh-my-zsh
+# zplug "plugins/zsh_reload", from:oh-my-zsh
 
 # plugins from github
 zplug "djui/alias-tips"
@@ -57,6 +66,7 @@ if zplug check b4b4r07/enhancd; then
 fi
 
 # custom settings
-set -o emacs
+# set -o emacs
+bindkey -e
 # custom alias
 source $ZSH_HOME/alias.zsh
