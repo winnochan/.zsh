c079b84e2ccb13bf479ae5e714e9e70d2101bf93
WinnoChan <winnochan@icloud.com>
2017-12-11 10:25:49 +0800

    update

---
+++ custom.zsh
@@ -0,0 +1,34 @@
+export ENHANCD_FILTER=fzy
+export BLOX_CONF__ONELINE=true
+export BLOX_BLOCK__CWD_TRUNC=0
+export BLOX_BLOCK__HOST_USER_SHOW_ALWAYS=true
+export BLOX_BLOCK__HOST_MACHINE_SHOW_ALWAYS=true
+
+export GTAGSLABEL=pygments
+export SPACESHIP_TIME_SHOW=true
+export EDITOR="emacsclient -t -a vim"
+export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=9'
+export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static
+export RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup
+export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
+
+# goenv
+if command -v goenv >/dev/null 2>&1; then
+    eval "$(goenv init -)"
+fi
+
+# nodenv
+if command -v nodenv >/dev/null 2>&1; then
+    eval "$(nodenv init -)"
+fi
+
+# pyenv
+if command -v pyenv >/dev/null 2>&1; then
+    eval "$(pyenv init -)"
+    # eval "$(pyenv virtualenv-init -)"
+fi
+
+# scalaenv
+if command -v scalaenv >/dev/null 2>&1; then
+    eval "$(scalaenv init -)"
+fi
---
+++ zgen.zsh
@@ -0,0 +1,47 @@
+export ZGEN_RESET_ON_CHANGE=($ZSH_HOME/zgen.zsh)
+export ZGEN_DIR=$ZSH_HOME/zgen
+source $ZGEN_DIR/zgen.zsh
+
+# if the init scipt doesn't exist
+if ! zgen saved; then
+
+    # from oh-my-zsh
+    zgen oh-my-zsh
+    zgen oh-my-zsh plugins/colored-man-pages
+    zgen oh-my-zsh plugins/git
+    zgen oh-my-zsh plugins/last-working-dir
+    zgen oh-my-zsh plugins/per-directory-history
+    zgen oh-my-zsh plugins/tig
+
+    # from github
+    # zgen load djui/alias-tips
+    zgen load hlissner/zsh-autopair
+    # zgen load unixorn/autoupdate-zgen
+    # zgen load supercrabtree/k
+    # zgen load RobSis/zsh-completion-generator
+    zgen load molovo/tipz
+
+    # theme
+    zgen load mafredri/zsh-async
+    # zgen load sindresorhus/pure
+    zgen load yardnsm/blox-zsh-theme
+
+    # from zsh-user
+    zgen load zsh-users/zaw
+    zgen load zsh-users/zsh-autosuggestions
+    zgen load zsh-users/zsh-completions
+    # zgen load zsh-users/zsh-syntax-highlighting
+    zgen load b4b4r07/enhancd
+    zgen load zdharma/fast-syntax-highlighting
+    zgen load zsh-users/zsh-history-substring-search
+
+    # generate the init script from plugins above
+    zgen save
+fi
+
+# autoload -U compinit && compinit
+
+# # custom config
+source $ZSH_HOME/alias.zsh
+source $ZSH_HOME/custom.zsh
+source $ZSH_HOME/fasd.zsh
