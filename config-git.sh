51621b2c979ee669038a1abf39e96654482bf663
WinnoChan <winnochan@icloud.com>
2017-12-30 00:09:00 +0800

    update config

--- config-git.sh
+++ config-git.sh
@@ -6,5 +6,6 @@ git config --global core.autocrlf input
 git config --global core.safecrlf true
 git config --global core.editor "emacsclient -t -a vim"
 git config --global core.precomposeunicode true
+git config --global core.quotepath false
 git config --global push.default simple
 git config --global pull.rebase false
