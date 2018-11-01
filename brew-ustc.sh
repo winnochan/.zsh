f579a2659d86e984edc19ce02273917896393a83
陈万豪 <cwh0594@175game.com>
2018-11-01 09:19:06 +0800

    update brew config

--- brew-ustc.sh
+++ brew-ustc.sh
@@ -1,6 +1,12 @@
 cd "$(brew --repo)"
 git remote set-url origin https://mirrors.ustc.edu.cn/brew.git
-cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
-git remote set-url origin https://mirrors.ustc.edu.cn/homebrew-core.git
-cd "$(brew --repo)/Library/Taps/caskroom/homebrew-cask"
-git remote set-url origin https://mirrors.ustc.edu.cn/homebrew-cask.git
+
+if [ -d $(brew --repo)/Library/Taps/homebrew/homebrew-core ]; then
+    cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+    git remote set-url origin https://mirrors.ustc.edu.cn/homebrew-core.git
+fi
+
+if [ -d $(brew --repo)/Library/Taps/caskroom/homebrew-cask ]; then
+    cd "$(brew --repo)/Library/Taps/caskroom/homebrew-cask"
+    git remote set-url origin https://mirrors.ustc.edu.cn/homebrew-cask.git
+fi
