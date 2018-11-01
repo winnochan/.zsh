3f08804f04ae3ca61d5244ed0bd54ff3d70c9276
陈万豪 <cwh0594@175game.com>
2018-11-01 09:19:06 +0800

    update brew config

--- brew-origin.sh
+++ brew-origin.sh
@@ -1,6 +1,12 @@
 cd "$(brew --repo)"
 git remote set-url origin https://github.com/Homebrew/brew.git
-cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
-git remote set-url origin https://github.com/Homebrew/homebrew-core.git
-cd "$(brew --repo)/Library/Taps/caskroom/homebrew-cask"
-git remote set-url origin https://github.com/caskroom/homebrew-cask.git
+
+if [ -d $(brew --repo)/Library/Taps/homebrew/homebrew-core ]; then
+    cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+    git remote set-url origin https://github.com/Homebrew/homebrew-core.git
+fi
+
+if [ -d $(brew --repo)/Library/Taps/caskroom/homebrew-cask ]; then
+    cd "$(brew --repo)/Library/Taps/caskroom/homebrew-cask"
+    git remote set-url origin https://github.com/caskroom/homebrew-cask.git
+fi
