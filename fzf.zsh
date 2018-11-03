96e63b93eb2afaca8a34ad60ad914e38a200523e
陈万豪 <winnochan@icloud.com>
2018-11-04 03:28:20 +0800

    add config

--- fzf.zsh
+++ fzf.zsh
@@ -1,14 +1,29 @@
-# Setup fzf
-# ---------
-if [[ ! "$PATH" == */home/winnochan/.linuxbrew/opt/fzf/bin* ]]; then
-  export PATH="$PATH:/home/winnochan/.linuxbrew/opt/fzf/bin"
+if [ "$(uname)" = "Darwin" ]; then
+    # Setup fzf
+    # ---------
+    if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+        export PATH="$PATH:/usr/local/opt/fzf/bin"
+    fi
+
+    # Auto-completion
+    # ---------------
+    [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+    # Key bindings
+    # ------------
+    source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+elif [ "$(uname)" = "Linux" ]; then
+    # Setup fzf
+    # ---------
+    if [[ ! "$PATH" == */home/winnochan/.linuxbrew/opt/fzf/bin* ]]; then
+        export PATH="$PATH:/home/winnochan/.linuxbrew/opt/fzf/bin"
+    fi
+
+    # Auto-completion
+    # ---------------
+    [[ $- == *i* ]] && source "/home/winnochan/.linuxbrew/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+    # Key bindings
+    # ------------
+    source "/home/winnochan/.linuxbrew/opt/fzf/shell/key-bindings.zsh"
 fi
-
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/home/winnochan/.linuxbrew/opt/fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "/home/winnochan/.linuxbrew/opt/fzf/shell/key-bindings.zsh"
-
