2210cde7815584ea89fdffea01d8ed388d1c6e74
陈万豪 <winnochan@icloud.com>
2018-11-03 13:02:37 +0800

    add config

--- custom.zsh
+++ custom.zsh
@@ -44,6 +44,10 @@ if command -v brew >/dev/null 2>&1; then
         export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
     elif [ "$(uname)" = "Linux" ]; then
         export HOMEBREW_BUILD_FROM_SOURCE=1
+
+        export C_INCLUDE_PATH="$HOME/.linuxbrew/include:$C_INCLUDE_PATH"
+        export CPLUS_INCLUDE_PATH="$HOME/.linuxbrew/include:$CPLUS_INCLUDE_PATH"
+        export LD_LIBRARY_PATH="$HOME/.linuxbrew/lib:$LD_LIBRARY_PATH"
     fi
 
     # ccache
