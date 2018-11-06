8229ba4ee98243a03eb4a92548fbeffb55c1b634
陈万豪 <cwh0594@175game.com>
2018-11-06 13:59:07 +0800

    update config

--- fzf.zsh
+++ fzf.zsh
@@ -16,14 +16,14 @@ elif [ "$(uname)" = "Linux" ]; then
     # Setup fzf
     # ---------
     if [[ ! "$PATH" == */home/winnochan/.linuxbrew/opt/fzf/bin* ]]; then
-        export PATH="$PATH:/home/winnochan/.linuxbrew/opt/fzf/bin"
+        export PATH="$PATH:$HOME/.linuxbrew/opt/fzf/bin"
     fi
 
     # Auto-completion
     # ---------------
-    [[ $- == *i* ]] && source "/home/winnochan/.linuxbrew/opt/fzf/shell/completion.zsh" 2> /dev/null
+    [[ $- == *i* ]] && source "$HOME/.linuxbrew/opt/fzf/shell/completion.zsh" 2> /dev/null
 
     # Key bindings
     # ------------
-    source "/home/winnochan/.linuxbrew/opt/fzf/shell/key-bindings.zsh"
+    source "$HOME/.linuxbrew/opt/fzf/shell/key-bindings.zsh"
 fi
