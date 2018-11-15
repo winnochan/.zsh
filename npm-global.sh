d7a029bb4fb5b4542e83cedf573dfc25e793f50e
陈万豪 <winnochan@icloud.com>
2018-11-16 01:24:34 +0800

    add config

--- npm-global.sh
+++ npm-global.sh
@@ -1,1 +1,1 @@
-npm i -g cordova express-generator forever node-gyp nrm gitbook-cli pkg nodejs-dashboard fastify-cli
+npm i -g cordova express-generator forever node-gyp nrm gitbook-cli pkg nodejs-dashboard fastify-cli babel-cli es-checker
