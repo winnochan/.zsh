caf98255cdb4cd789b2c19ad57c4412e56ab971a
WinnoChan <winnochan@icloud.com>
2018-11-22 11:17:24 +0800

    update config

--- zgen.zsh
+++ zgen.zsh
@@ -10,13 +10,13 @@ if ! zgen saved; then
 
     # from oh-my-zsh
     zgen oh-my-zsh
-    zgen oh-my-zsh plugins/colored-man-pages
+    # zgen oh-my-zsh plugins/colored-man-pages
     zgen oh-my-zsh plugins/fasd
-    zgen oh-my-zsh plugins/fzf
+    # zgen oh-my-zsh plugins/fzf
     zgen oh-my-zsh plugins/git
     zgen oh-my-zsh plugins/last-working-dir
-    zgen oh-my-zsh plugins/per-directory-history
-    zgen oh-my-zsh plugins/tig
+    # zgen oh-my-zsh plugins/per-directory-history
+    # zgen oh-my-zsh plugins/tig
 
     # from github
     # zgen load djui/alias-tips
@@ -30,15 +30,15 @@ if ! zgen saved; then
     zgen load mafredri/zsh-async
     # zgen load sindresorhus/pure
     zgen load yardnsm/blox-zsh-theme
-    zgen load seletskiy/zsh-fuzzy-search-and-edit
+    # zgen load seletskiy/zsh-fuzzy-search-and-edit
     # zgen load urbainvaes/fzf-marks
 
     # from zsh-user
-    zgen load zsh-users/zaw
+    # zgen load zsh-users/zaw
     zgen load zsh-users/zsh-autosuggestions
     zgen load zsh-users/zsh-completions
     # zgen load zsh-users/zsh-syntax-highlighting
-    zgen load b4b4r07/enhancd
+    # zgen load b4b4r07/enhancd
     # zgen load changyuheng/fz fz.plugin.zsh
     zgen load hlissner/zsh-autopair
     # zgen load zdharma/fast-syntax-highlighting
