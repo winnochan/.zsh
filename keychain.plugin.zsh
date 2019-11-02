0347b5b271f72dcf7d0d3cf6b1c3151eacbbe6b8
陈万豪 <winnochan@icloud.com>
2019-11-02 10:49:50 +0800

    add keychain plugin

---
+++ keychain.plugin.zsh
@@ -0,0 +1,28 @@
+if command -v keychain >/dev/null 2>&1; then
+
+    # if [ -f ~/.ssh/ecdsa ] && [ -f ~/.ssh/dsa ] && [ -f ~/.ssh/rsa ]; then
+    if [ -f ~/.ssh/ecdsa ]; then
+
+        # echo 'has ecdsa, dsa, rsa and keychain'
+
+        keychain_file=$HOME/.keychain/$(hostname)-sh
+        if [ -f $keychain_file ]; then
+            source $keychain_file
+        fi
+
+        cur_ssh_agent_id=$(ps x | grep ssh-agent | grep -v grep | cut -d ' ' -f 1)
+        if [[ ${cur_ssh_agent_id} != ${SSH_AGENT_PID} ]]; then
+
+            # echo 'keychain config is old'
+
+            keychain -q ~/.ssh/ecdsa
+            # keychain -q ~/.ssh/dsa
+            # keychain -q ~/.ssh/rsa
+
+            source $keychain_file
+
+        fi
+
+    fi
+
+fi
