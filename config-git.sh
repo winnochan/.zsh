1ef265c21766adee83cd4e480be573996eadfe1f
陈万豪 <winnochan@icloud.com>
2019-11-03 14:28:00 +0800

    remove unused file

--- config-git.sh
+++ config-git.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-git config --global user.name "WinnoChan"
+git config --global user.name "Winno Chan"
 git config --global user.email "winnochan@icloud.com"
-git config --global core.autocrlf input
-git config --global core.safecrlf true
-git config --global core.editor "emacsclient -t -a vim"
+git config --global core.editor "vim"
 git config --global core.precomposeunicode true
 git config --global core.quotepath false
-git config --global push.default simple
-git config --global pull.rebase false
--- npm-global.sh
+++
@@ -1,4 +0,0 @@
-npm i -g cordova express-generator forever node-gyp nrm
-npm i -g gitbook-cli pkg nodejs-dashboard
-npm i -g yarn react-native-cli fastify-cli babel-cli es-checker
-npm i -g tern js-beautify eslint babel-eslint eslint-plugin-react
