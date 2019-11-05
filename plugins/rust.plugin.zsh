fa84f77c2ceed89a727a2baaf6876c9d94f86700
陈万豪 <winnochan@icloud.com>
2019-11-05 15:25:54 +0800

    optimize rust config

--- plugins/rust.plugin.zsh
+++ plugins/rust.plugin.zsh
@@ -4,7 +4,17 @@ if [ -f $cargo_env ]; then
 fi
 
 if command -v rustup >/dev/null 2>&1; then
-    export RUST_SRC_PATH=$(rustc --print sysroot)/lib/rustlib/src/rust/src
+    rust_cache=$ZSH_CACHE/rust
+    if [ ! -d $rust_cache ]; then
+        mkdir -p $rust_cache
+    fi
+
+    if [ ! -f $rust_cache/rust_sysroot.zsh ]; then
+        echo "export RUST_SYSROOT=$(rustc --print sysroot)" > $rust_cache/rust_sysroot.zsh
+    fi
+    source $rust_cache/rust_sysroot.zsh
+
+    export RUST_SRC_PATH=$RUST_SYSROOT/lib/rustlib/src/rust/src
 
     export RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup
 fi
