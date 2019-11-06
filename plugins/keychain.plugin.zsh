573059d3319000f3294b42a4a1674453cdaf2b88
陈万豪 <winnochan@icloud.com>
2019-11-06 10:32:00 +0800

    optimize keychain plugin

--- plugins/keychain.plugin.zsh
+++ plugins/keychain.plugin.zsh
@@ -8,23 +8,12 @@ if command -v keychain >/dev/null 2>&1; then
             source $keychain_file
         fi
 
-        cur_ssh_agent_id=0
-        if [ "$ZSH_SYS" = "Darwin" ]; then
-            cur_ssh_agent_id=$(pgrep ssh-agent)
-        else
-            cur_ssh_agent_id=$(pidof ssh-agent)
-        fi
-
-        if [[ ${cur_ssh_agent_id} != ${SSH_AGENT_PID} ]]; then
-
+        if [[ $(ps -p $SSH_AGENT_PID | grep -v PID) != '' ]]; then
             keychain -q ~/.ssh/ecdsa
             # keychain -q ~/.ssh/dsa
             # keychain -q ~/.ssh/rsa
-
             source $keychain_file
-
         fi
-
     fi
 
 fi
