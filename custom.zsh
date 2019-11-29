e37b34e3775eeee5832f11a56ce7816826ca147e
陈万豪 <winnochan@icloud.com>
2019-11-29 23:43:14 +0800

    remove unused scripts

--- custom.zsh
+++ custom.zsh
@@ -1,20 +1,10 @@
-# gcloud completion
-# gcloud_completion_path=$ZSH_DIR/.zgen/littleq0903/gcloud-zsh-completion-master/src
-# if [ -f $gcloud_completion_path ]; then
-#     fpath=($gcloud_completion_path $fpath)
-# fi
-
-# if command -v pipenv >/dev/null 2>&1; then
-#     eval "$(pipenv --completion)"
-# fi
-
 # fasd
-# if [ "$ZSH_SYS" = "Darwin" ]; then
-#     alias o='a -e open_command'
-# fi
+if [ "$ZSH_SYS" = "Darwin" ]; then
+    alias o='a -e open_command'
+fi
 
 # editor
-export EDITOR="emacsclient -t -a vim"
+export EDITOR="vim"
 
 export HISTSIZE=1000000
 export HISEFILESIZE=-1
