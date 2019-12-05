674964a581dc7d3959331c25115bfc1dda40612c
陈万豪 <winnochan@icloud.com>
2019-12-05 19:59:09 +0800

    update  flutter config

--- plugins/flutter.plugin.zsh
+++ plugins/flutter.plugin.zsh
@@ -2,8 +2,8 @@
 export PATH="$HOME/.pub-cache/bin:$PATH"
 
 # flutter
-export PATH="$HOME/flutter/bin:$PATH"
-export PUB_HOSTED_URL="https://pub.flutter-io.cn"
-export FLUTTER_STORAGE_BASE_URL="https://storage.flutter-io.cn"
+export PATH="$ZSH_CACHE/flutter/bin:$PATH"
+# export PUB_HOSTED_URL="https://pub.flutter-io.cn"
+# export FLUTTER_STORAGE_BASE_URL="https://storage.flutter-io.cn"
 export PUB_HOSTED_URL="https://mirrors.tuna.tsinghua.edu.cn/dart-pub/"
 export FLUTTER_STORAGE_BASE_URL="https://mirrors.tuna.tsinghua.edu.cn/flutter"
