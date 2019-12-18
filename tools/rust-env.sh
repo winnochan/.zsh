6625dc7d524f6930708db1444a593f266ed295b5
winnochan <winnochan@icloud.com>
2019-12-18 19:19:20 +0800

    update zsh config

--- tools/rust-env.sh
+++ tools/rust-env.sh
@@ -2,4 +2,4 @@ if ! command -v rustup >/dev/null 2>&1; then
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
 
-rustup component add cargo clippy llvm-tools-preview rls rust-analysis rust-docs rust-src rust-std rustc rustfmt
+rustup component add llvm-tools-preview rls rust-analysis rust-src
