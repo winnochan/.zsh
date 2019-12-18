5aea1913d26096f2ed71da82004c839db56747b8
winnochan <winnochan@icloud.com>
2019-12-18 19:19:20 +0800

    update zsh config

--- zgen.zsh
+++ zgen.zsh
@@ -68,7 +68,7 @@ if ! zgen saved; then
     zgen load hlissner/zsh-autopair
     zgen load zdharma/fast-syntax-highlighting
     # zgen load zsh-users/zsh-syntax-highlighting
-    # zgen load zsh-users/zsh-history-substring-search
+    zgen load zsh-users/zsh-history-substring-search
 
     # generate the init script from plugins above
     zgen save
