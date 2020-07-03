f677e178df763124d5a6cc46316c654a917da648
陈万豪 <winnochan@icloud.com>
2020-07-03 22:58:46 +0800

    optimize flutter plugin

--- plugins/flutter.plugin.zsh
+++ plugins/flutter.plugin.zsh
@@ -1,9 +1,21 @@
 # dart
-export PATH="$HOME/.pub-cache/bin:$PATH"
+if [ -d $HOME/.pub-cache ]; then
+    export PATH="$HOME/.pub-cache/bin:$PATH"
+fi
 
 # flutter
-export PATH="$ZSH_CACHE/flutter/bin:$PATH"
-# export PUB_HOSTED_URL="https://pub.flutter-io.cn"
-# export FLUTTER_STORAGE_BASE_URL="https://storage.flutter-io.cn"
-export PUB_HOSTED_URL="https://mirrors.tuna.tsinghua.edu.cn/dart-pub/"
-export FLUTTER_STORAGE_BASE_URL="https://mirrors.tuna.tsinghua.edu.cn/flutter"
+if [ -d $ZSH_CACHE/flutter ]; then
+    export FLUTTER_HOME=$ZSH_CACHE/flutter
+
+    export PATH="$FLUTTER_HOME/bin:$PATH"
+
+    if [ -d $FLUTTER_HOME/bin/cache/dart-sdk ]; then
+        export DART_SDK_HOME=$FLUTTER_HOME/bin/cache/dart-sdk
+        export PATH="${DART_SDK_HOME}/bin:$PATH"
+    fi
+
+    # export PUB_HOSTED_URL="https://pub.flutter-io.cn"
+    # export FLUTTER_STORAGE_BASE_URL="https://storage.flutter-io.cn"
+    export PUB_HOSTED_URL="https://mirrors.tuna.tsinghua.edu.cn/dart-pub/"
+    export FLUTTER_STORAGE_BASE_URL="https://mirrors.tuna.tsinghua.edu.cn/flutter"
+fi
