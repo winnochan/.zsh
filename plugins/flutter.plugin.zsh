4f9c73360721dc9bbbc35392dc92559ddff4275b
陈万豪 <winnochan@icloud.com>
2020-07-06 23:36:00 +0800

    fix flutter path error

--- plugins/flutter.plugin.zsh
+++ plugins/flutter.plugin.zsh
@@ -1,21 +1,19 @@
 # dart
 if [ -d $HOME/.pub-cache ]; then
-    export PATH="$HOME/.pub-cache/bin:$PATH"
+    export PATH=$HOME/.pub-cache/bin:$PATH
 fi
 
 # flutter
 if [ -d $ZSH_CACHE/flutter ]; then
     export FLUTTER_ROOT=$ZSH_CACHE/flutter
 
-    export PATH="$${FLUTTER_ROOT}/bin:$PATH"
+    export PATH=${FLUTTER_ROOT}/bin:$PATH
 
     if [ -d ${FLUTTER_ROOT}/bin/cache/dart-sdk ]; then
         export DART_SDK_HOME=${FLUTTER_ROOT}/bin/cache/dart-sdk
-        export PATH="${DART_SDK_HOME}/bin:$PATH"
+        export PATH=${DART_SDK_HOME}/bin:$PATH
     fi
 
-    # export PUB_HOSTED_URL="https://pub.flutter-io.cn"
-    # export FLUTTER_STORAGE_BASE_URL="https://storage.flutter-io.cn"
-    export PUB_HOSTED_URL="https://mirrors.tuna.tsinghua.edu.cn/dart-pub/"
-    export FLUTTER_STORAGE_BASE_URL="https://mirrors.tuna.tsinghua.edu.cn/flutter"
+    export PUB_HOSTED_URL=https://mirrors.tuna.tsinghua.edu.cn/dart-pub/
+    export FLUTTER_STORAGE_BASE_URL=https://mirrors.tuna.tsinghua.edu.cn/flutter
 fi
