28c0d0476db479fe8ae995c45c65beec2e37cde6
winnochan <winnochan@icloud.com>
2020-08-25 11:53:51 +0800

    udpate asdf plugin

--- tools/asdf-plugins.sh
+++ tools/asdf-plugins.sh
@@ -1,8 +1,3 @@
-asdf plugin-add flutter
-asdf plugin add gcloud
 asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
-asdf plugin-add java
+asdf plugin-add java https://github.com/halcyon/asdf-java.git
 asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf plugin-add redis https://github.com/smashedtoatoms/asdf-redis.git
-asdf plugin-add rust https://github.com/code-lever/asdf-rust.git
-asdf plugin-add python
