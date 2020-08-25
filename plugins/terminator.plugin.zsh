1c5e8c2ecd69860285a669e509f8aa007ea4f4f8
winnochan <winnochan@icloud.com>
2020-08-25 14:21:28 +0800

    add terminator config

--- plugins/terminator.plugin.zsh
+++ plugins/terminator.plugin.zsh
@@ -1,11 +1,11 @@
-if [ ! -f $HOME/.config/terminator/config ]; then
-    if [ ! -d $HOME/.config/terminator ]; then
-        mkdir -p $HOME/.config/terminator
-    fi
-    touch $HOME/.config/terminator/config
-fi
+# if [ ! -f $HOME/.config/terminator/config ]; then
+#     if [ ! -d $HOME/.config/terminator ]; then
+#         mkdir -p $HOME/.config/terminator
+#     fi
+#     touch $HOME/.config/terminator/config
+# fi
 
-if [ "$(diff ~/.config/terminator/config ~/.zsh/config/terminator.config)" != '' ]; then
-    rm $HOME/.config/terminator/config
-    ln -s $ZSH_DIR/config/terminator.config $HOME/.config/terminator/config
-fi
+# if [ "$(diff ~/.config/terminator/config ~/.zsh/config/terminator.config)" != '' ]; then
+#     rm $HOME/.config/terminator/config
+#     ln -s $ZSH_DIR/config/terminator.config $HOME/.config/terminator/config
+# fi
