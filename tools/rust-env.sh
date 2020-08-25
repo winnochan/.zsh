d5d418dc504d5962598a555ecb9f8e8173b790c0
winnochan <winnochan@icloud.com>
2020-08-25 10:49:51 +0800

    fix rust env

--- tools/rust-env.sh
+++ tools/rust-env.sh
@@ -4,8 +4,13 @@ if [ ! -d $HOME/.rustup ]; then
 fi
 
 if command -v rustup >/dev/null 2>&1; then
-    rustup component add llvm-tools-preview rls rust-analysis rust-src
+    rustup component add llvm-tools-preview rls rust-analysis rust-src rustfmt clippy
 elif [ -f $ZSH_DIR/plugins/rust.plugin.zsh ]; then
     source $ZSH_DIR/plugins/rust.plugin.zsh
-    rustup component add llvm-tools-preview rls rust-analysis rust-src
+    rustup component add llvm-tools-preview rls rust-analysis rust-src rustfmt clippy
+fi
+
+if command -v cargo >/dev/null 2>&1; then
+    cargo install cargo-edit
+    cargo install cargo-audit
 fi
