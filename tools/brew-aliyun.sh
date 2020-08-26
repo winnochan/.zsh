bd42f8e9a7d0e242543af49c4fce4cd92a5075c5
陈万豪 <winnochan@icloud.com>
2020-08-26 23:58:31 +0800

    add brew aliyun mirror

---
+++ tools/brew-aliyun.sh
@@ -0,0 +1,11 @@
+if [ -d $(brew --repo) ]; then
+    git -C "$(brew --repo)" remote set-url origin https://mirrors.aliyun.com/homebrew/brew.git
+fi
+
+if [ -d $(brew --repo homebrew/core) ]; then
+    git -C "$(brew --repo homebrew/core)" remote set-url origin https://mirrors.aliyun.com/homebrew/homebrew-core.git
+fi
+
+if [ -d $(brew --repo homebrew/cask) ]; then
+    git -C "$(brew --repo homebrew/cask)" remote set-url origin https://mirrors.sjtug.sjtu.edu.cn/git/homebrew-cask.git
+fi
