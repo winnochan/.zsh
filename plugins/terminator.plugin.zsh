58a1af40c4b0556b18ca10affa09e53b058e1440
winnochan <winnochan@icloud.com>
2020-08-26 10:36:01 +0800

    update terminator config

--- plugins/terminator.plugin.zsh
+++ plugins/terminator.plugin.zsh
@@ -1,11 +1,6 @@
-# if [ ! -f $HOME/.config/terminator/config ]; then
-#     if [ ! -d $HOME/.config/terminator ]; then
-#         mkdir -p $HOME/.config/terminator
-#     fi
-#     touch $HOME/.config/terminator/config
-# fi
-
-# if [ "$(diff ~/.config/terminator/config ~/.zsh/config/terminator.config)" != '' ]; then
-#     rm $HOME/.config/terminator/config
-#     ln -s $ZSH_DIR/config/terminator.config $HOME/.config/terminator/config
-# fi
+if [ ! -L $HOME/.config/terminator ]; then
+    if [ -e $HOME/.config/terminator ]; then
+        rm -rf $HOME/.config/terminator
+    fi
+    ln -s $ZSH_DIR/config/terminator $HOME/.config/terminator
+fi
