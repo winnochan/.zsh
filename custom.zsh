7f87c4cf3cbd280bec969287452575790d9bf26b
winnochan <winnochan@icloud.com>
2020-09-02 17:10:21 +0800

    enable auto rehash

--- custom.zsh
+++ custom.zsh
@@ -11,3 +11,7 @@ export HISTSIZE=10000
 export SAVEHIST=1000
 setopt SHARE_HISTORY
 setopt INC_APPEND_HISTORY
+
+# if the init scipt doesn't exist
+zstyle :omz:plugins:ssh-agent agent-forwarding on
+zstyle ':completion:*' rehash true
