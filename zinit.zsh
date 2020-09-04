856effc2dd3b3392eb81533f9424df35c90dcb8e
winnochan <winnochan@icloud.com>
2020-09-04 14:11:55 +0800

    update zinit config

--- zinit.zsh
+++ zinit.zsh
@@ -13,18 +13,8 @@ ZINIT[OPTIMIZE_OUT_DISK_ACCESSES]=0
 
 source ${ZINIT[BIN_DIR]}/zinit.zsh
 
-# export ALL_PROXY=http://127.0.0.1:1087;
-
-# blox theme
-zinit ice silent from=gitee.com src=blox.zsh
-zinit light winnochan/blox-zsh-theme
-
 # 记住上次的工作目录
 # zinit snippet OMZ::plugins/last-working-dir/last-working-dir.plugin.zsh
-
-# 自动推荐历史命令插件
-zinit ice silent wait lucid atload=_zsh_autosuggest_start from=gitee.com
-zinit light winnochan/zsh-autosuggestions
 
 # 快速目录跳转插件
 # zinit ice silent wait'0a'
@@ -34,21 +24,9 @@ zinit light winnochan/zsh-autosuggestions
 # zinit ice silent wait'0b'
 # zinit snippet OMZ::plugins/git/git.plugin.zsh
 
-# # 隔离每个工作目录的命令历史记录
-# zinit ice silent wait'0c'
-# zinit snippet OMZ::plugins/per-directory-history/per-directory-history.zsh
-
 # oh my zsh lib config
 # zinit ice silent wait=1a src='lib/*.zsh' pick=/dev/null from=gitee.com
 # zinit snippet OMZL::git.zsh
-
-# 自动括号, 引号匹配插件
-zinit ice silent wait=2a from
-zinit light winnochan/zsh-autopair
-
-# # 语法高亮插件
-# zinit ice silent wait'1d'
-# zinit light zdharma/fast-syntax-highlighting
 
 # # fzf 模糊文件查找工具, 延迟2秒加载
 # zinit ice silent wait'2a' from"gh-r" as"program"
@@ -69,9 +47,52 @@ zinit light winnochan/zsh-autopair
 # zinit ice silent wait'3b'
 # zinit snippet OMZ::plugins/extract/extract.plugin.zsh
 
+# zinit ice silent from=gitee.com multisrc='lib/*.zsh'; zinit light winnochan/ohmyzsh
+# zinit ice silent from=gitee.com src='plugins/alias-finder/*.plugin.zsh'; zinit light winnochan/ohmyzsh
+# zinit ice silent from=gitee.com src='plugins/extract/*.plugin.zsh'; zinit light winnochan/ohmyzsh
+# zinit ice silent from=gitee.com src='plugins/fasd/*.plugin.zsh'; zinit light winnochan/ohmyzsh
+# zinit ice silent from=gitee.com src='plugins/git/*.plugin.zsh'; zinit light winnochan/ohmyzsh
+# zinit ice silent from=gitee.com src='plugins/last-working-dir/*.plugin.zsh'; zinit light winnochan/ohmyzsh
+# zinit snippet https://gitee.com/winnochan/ohmyzsh/raw/master/lib/cli.zsh
+# zinit snippet https://gitee.com/winnochan/ohmyzsh/raw/master/lib/directories.zsh
+# zinit snippet https://gitee.com/winnochan/ohmyzsh/raw/master/lib/git.zsh
+# zinit snippet https://gitee.com/winnochan/ohmyzsh/raw/master/lib/key-bindings.zsh
+zinit ice silent svn multisrc='*.zsh'; zinit snippet OMZ::lib
+# zinit ice silent from=gitee.com src=oh-my-zsh.sh; zinit light winnochan/ohmyzsh
+zinit snippet https://gitee.com/winnochan/ohmyzsh/raw/master/plugins/alias-finder/alias-finder.plugin.zsh
+zinit snippet https://gitee.com/winnochan/ohmyzsh/raw/master/plugins/extract/extract.plugin.zsh
+zinit snippet https://gitee.com/winnochan/ohmyzsh/raw/master/plugins/fasd/fasd.plugin.zsh
+zinit snippet https://gitee.com/winnochan/ohmyzsh/raw/master/plugins/git/git.plugin.zsh
+zinit snippet https://gitee.com/winnochan/ohmyzsh/raw/master/plugins/last-working-dir/last-working-dir.plugin.zsh
+
+# fzf and fasd config
+# zinit ice silent wait=1a from=gitee.com multisrc='shell/*.zsh'; zinit light winnochan/fzf
+# zinit ice silent wait=1b from=gitee.com; zinit light winnochan/fzf-fasd
+
+# blox theme
+zinit ice silent from=gitee.com src=blox.zsh; zinit light winnochan/blox-zsh-theme
+
+# 自动推荐历史命令插件
+zinit ice silent wait=0 atload=_zsh_autosuggest_start from=gitee.com; zinit light winnochan/zsh-autosuggestions
+
 # 大量常用命令的补全插件
-zinit ice silent blockf wait=3c from=gitee.com
-zinit light winnochan/zsh-completions
+zinit ice silent blockf from=gitee.com; zinit light winnochan/zsh-completions
+
+# 自动括号, 引号匹配插件
+zinit ice silent wait=1a from=gitee.com; zinit light winnochan/zsh-autopair
+
+# 语法高亮插件
+zinit ice silent wait=1b from=gitee.com; zinit light winnochan/fast-syntax-highlighting
+
+# zinit wait lucid from=gitee.com for \
+#       atinit="ZINIT[COMPINIT_OPTS]=-C; zicompinit; zicdreplay" \
+#       winnochan/zsh-autopair \
+#       winnochan/fast-syntax-highlighting \
+#       blockf \
+#       winnochan/zsh-completions \
+#       atload="!_zsh_autosuggest_start" \
+#       winnochan/zsh-autosuggestions
+
 
 ###############################################################################
 # 自定义的插件和补全
