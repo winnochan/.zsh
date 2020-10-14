be9a72c48073a16f8f0f49f1daeb2d071dbc7777
陈万豪 <winnochan@icloud.com>
2020-10-14 22:54:31 +0800

    use asdf to install python

--- tools/asdf-plugins.sh
+++ tools/asdf-plugins.sh
@@ -4,3 +4,6 @@ asdf install golang $(asdf latest golang)
 asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
 asdf install nodejs $(asdf latest nodejs 12)
+
+asdf plugin-add python
+asdf install python $(asdf latest python 3)
