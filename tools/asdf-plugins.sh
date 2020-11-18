e9583768b622a0c32ab79284831936766626351c
winnochan <winnochan@icloud.com>
2020-11-19 01:39:57 +0800

    update

--- tools/asdf-plugins.sh
+++ tools/asdf-plugins.sh
@@ -5,7 +5,7 @@ asdf install golang $(asdf latest golang)
 
 asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
-asdf install nodejs $(asdf latest nodejs 12)
+asdf install nodejs $(asdf latest nodejs 14)
 
 asdf plugin-add python
 asdf install python $(asdf latest python 3)
