967879df2fe9ad7766dc400cfb655cfbd99592d2
winnochan <winnochan@icloud.com>
2020-11-19 23:33:32 +0800

    update

--- plugins/go.plugin.zsh
+++ plugins/go.plugin.zsh
@@ -1,7 +1,7 @@
-export GOPATH=$HOME/go
+export GOPATH=$ZSH_CACHE/go
 export GO111MODULE=on
 export GOPROXY=https://goproxy.io,direct
-export GOSUMDB=gosum.io+ce6e7565+AY5qEHUk/qmHc5btzW45JVoENfazw8LielDsaI+lEbq6
+# export GOSUMDB=gosum.io+ce6e7565+AY5qEHUk/qmHc5btzW45JVoENfazw8LielDsaI+lEbq6
 # export GOPRIVATE=*.corp.example.com
 
 # godoc color
