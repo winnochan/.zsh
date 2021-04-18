e297ef4c0f72275b5e8d52cf6b0ea527547f6a94
winnochan <winnochan@icloud.com>
2021-04-18 16:01:11 +0800

    upload xray dat

---
+++ xray/update.sh
@@ -0,0 +1,2 @@
+wget -O $ZSH_DIR/xray/geoip.dat https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat
+wget -O $ZSH_DIR/xray/geosite.dat https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat
