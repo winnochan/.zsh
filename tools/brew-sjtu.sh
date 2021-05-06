f77e65c797dabdecc73569c5ede75b7a6c7adb05
winnochan <winnochan@icloud.com>
2021-05-06 14:32:35 +0800

    update config

--- tools/brew-sjtu.sh
+++ tools/brew-sjtu.sh
@@ -3,9 +3,15 @@ if [ -d $(brew --repo) ]; then
 fi
 
 if [ -d $(brew --repo homebrew/core) ]; then
+if [ "$ZSH_SYS" = "Darwin" ]; then
     git -C "$(brew --repo homebrew/core)" remote set-url origin https://mirrors.sjtug.sjtu.edu.cn/git/homebrew-core.git
+    elif [ "$ZSH_SYS" = "Linux" ]; then
+    git -C "$(brew --repo homebrew/core)" remote set-url origin https://mirrors.sjtug.sjtu.edu.cn/git/linuxbrew-core.git
+fi
 fi
 
+if [ "$ZSH_SYS" = "Darwin" ]; then
 if [ -d $(brew --repo homebrew/cask) ]; then
     git -C "$(brew --repo homebrew/cask)" remote set-url origin https://mirrors.sjtug.sjtu.edu.cn/git/homebrew-cask.git
+fi
 fi
--- tools/brew-tsinghua.sh
+++ tools/brew-tsinghua.sh
@@ -3,9 +3,15 @@ if [ -d $(brew --repo) ]; then
 fi
 
 if [ -d $(brew --repo homebrew/core) ]; then
+    if [ "$ZSH_SYS" = "Darwin" ]; then
     git -C "$(brew --repo homebrew/core)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
+    elif [ "$ZSH_SYS" = "Linux" ]; then
+    git -C "$(brew --repo homebrew/core)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/linuxbrew-core.git
+    fi
 fi
 
+if [ "$ZSH_SYS" = "Darwin" ]; then
 if [ -d $(brew --repo homebrew/cask) ]; then
     git -C "$(brew --repo homebrew/cask)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask.git
+fi
 fi
