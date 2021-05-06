6234a75356dfbe59fb5db3a678ff222fcc1e60ea
winnochan <winnochan@icloud.com>
2021-05-06 14:55:55 +0800

    update config

--- tools/rust-env.sh
+++ tools/rust-env.sh
@@ -1,15 +1,18 @@
 if [ ! -d $HOME/.rustup ]; then
-# if ! command -v rustup >/dev/null 2>&1; then
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  # if ! command -v rustup >/dev/null 2>&1; then
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
 
 if command -v rustup >/dev/null 2>&1; then
-    rustup component add llvm-tools-preview rls rust-analysis rust-src rustfmt clippy
+  rustup component add llvm-tools-preview rls rust-analysis rust-src rustfmt clippy
+  rustup toolchain add nightly
 elif [ -f $ZSH_DIR/plugins/rust.plugin.zsh ]; then
-    source $ZSH_DIR/plugins/rust.plugin.zsh
-    rustup component add llvm-tools-preview rls rust-analysis rust-src rustfmt clippy
+  source $ZSH_DIR/plugins/rust.plugin.zsh
+  rustup component add llvm-tools-preview rls rust-analysis rust-src rustfmt clippy
+  rustup toolchain add nightly
 fi
 
 if command -v cargo >/dev/null 2>&1; then
   cargo install cargo-edit cargo-audit cargo-outdated
+  cargo +nightly install racer
 fi
