a713ae82ee7c79aac458c6a96889b585d027e674
winnochan <winnochan@icloud.com>
2021-05-07 23:08:32 +0800

    use ustc brew mirror

--- plugins/brew.plugin.zsh
+++ plugins/brew.plugin.zsh
@@ -32,12 +32,8 @@ if command -v brew >/dev/null 2>&1; then
   #     export HOMEBREW_GITHUB_API_TOKEN=$(cat $HOME/.ssh/tokens/github-repo.token)
   # fi
   if [ "$ZSH_SYS" = "Darwin" ]; then
-    # export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.sjtug.sjtu.edu.cn/homebrew-bottles
-    # export HOMEBREW_BOTTLE_DOMAIN=https://mirror.sjtu.edu.cn/homebrew-bottles
-    # export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
+    export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
   elif [ "$ZSH_SYS" = "Linux" ]; then
-    # export HOMEBREW_BOTTLE_DOMAIN=https://mirror.sjtu.edu.cn/linuxbrew-bottles
-    # export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/linuxbrew-bottles
     export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/linuxbrew-bottles
   fi
 
