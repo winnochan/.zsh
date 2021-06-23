238dc997c912860672f8e2c7a6bcd2de210df0b7
WinnoChan <winnochan@icloud.com>
2021-06-23 14:22:15 +0800

    update git config

--- plugins/git.plugin.zsh
+++ plugins/git.plugin.zsh
@@ -1,1 +1,9 @@
+if [ "$(git config --global user.name)" != 'WinnoChan' ]; then
+  git config --global user.name WinnoChan
+fi
+
+if [ "$(git config --global user.email)" != 'winnochan@icloud.com' ]; then
+  git config --global user.email 'winnochan@icloud.com'
+fi
+
 export GIT_ASKPASS=`which ksshaskpass`
