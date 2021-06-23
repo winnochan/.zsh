8ff44f6bb9aeff2e544e1af820b7f142d2a3d251
WinnoChan <winnochan@icloud.com>
2021-06-23 15:31:50 +0800

    update git config

--- plugins/git.plugin.zsh
+++ plugins/git.plugin.zsh
@@ -3,10 +3,12 @@ if [ ! -f ~/.gitconfig ]; then
   git config --global user.name WinnoChan
   git config --global user.email 'winnochan@icloud.com'
 
-  if command -v ksshaskpass >/dev/null 2>&1; then
-    export GIT_ASKPASS=`which ksshaskpass`
-  else
+  if ! command -v ksshaskpass >/dev/null 2>&1; then
     git config --global credential.helper 'cache --timeout=604800'
   fi
 
+fi
+
+if command -v ksshaskpass >/dev/null 2>&1; then
+  export GIT_ASKPASS=`which ksshaskpass`
 fi
