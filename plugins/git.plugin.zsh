b76a34cf1b46d28dde85fbce5857c3ee6ba2e615
WinnoChan <winnochan@icloud.com>
2021-06-29 17:01:24 +0800

    update brew config

--- plugins/git.plugin.zsh
+++ plugins/git.plugin.zsh
@@ -7,6 +7,10 @@ if [ ! -f ~/.gitconfig ]; then
     git config --global credential.helper 'cache --timeout=604800'
   fi
 
+  git config --global url."https://mirrors.ustc.edu.cn/homebrew-core.git".insteadOf "https://github.com/Homebrew/homebrew-core"
+git config --global url."https://mirrors.ustc.edu.cn/linuxbrew-core.git".insteadOf "https://github.com/Homebrew/linuxbrew-core"
+git config --global url."https://mirrors.ustc.edu.cn/brew.git".insteadOf "https://github.com/Homebrew/brew"
+
 fi
 
 if command -v ksshaskpass >/dev/null 2>&1; then
