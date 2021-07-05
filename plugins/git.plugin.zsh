837b35801aa1ce2e3f73455140fac3e02fcf0d46
WinnoChan <winnochan@icloud.com>
2021-07-05 11:38:44 +0800

    add git config

--- plugins/git.plugin.zsh
+++ plugins/git.plugin.zsh
@@ -1,18 +1,9 @@
-if [ ! -f ~/.gitconfig ]; then
-
-  git config --global user.name WinnoChan
-  git config --global user.email 'winnochan@icloud.com'
-
-  if ! command -v ksshaskpass >/dev/null 2>&1; then
-    git config --global credential.helper 'cache --timeout=604800'
+if [ "$USER" = "winnochan" ]; then
+  if [ ! -L $HOME/.gitconfig ]; then
+    if [ -e $HOME/.gitconfig ]; then
+      rm -rf $HOME/.gitconfig
+    fi
+    ln -s $ZSH_DIR/config/gitconfig $HOME/.gitconfig
+    echo 'link git config'
   fi
-
-  git config --global url."https://mirrors.ustc.edu.cn/homebrew-core.git".insteadOf "https://github.com/Homebrew/homebrew-core"
-  git config --global url."https://mirrors.ustc.edu.cn/linuxbrew-core.git".insteadOf "https://github.com/Homebrew/linuxbrew-core"
-  git config --global url."https://mirrors.ustc.edu.cn/brew.git".insteadOf "https://github.com/Homebrew/brew"
-
-fi
-
-if command -v ksshaskpass >/dev/null 2>&1; then
-  export GIT_ASKPASS=`which ksshaskpass`
 fi
