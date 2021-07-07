79ddbd5a42a824e0f718fc1865651264e96315f0
WinnoChan <winnochan@icloud.com>
2021-07-07 16:50:55 +0800

    remove unused tools

--- tools/git-config.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-git config --global user.name "Winno Chan"
-git config --global user.email "winnochan@icloud.com"
-git config --global core.editor "vim"
-git config --global core.precomposeunicode true
-git config --global core.quotepath false
