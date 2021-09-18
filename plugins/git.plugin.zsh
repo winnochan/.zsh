924c3bb369707b8ded15f9596497af5f51cbc7d1
WinnoChan <winnochan@icloud.com>
2021-09-18 19:52:01 +0800

    update git config

--- plugins/git.plugin.zsh
+++ plugins/git.plugin.zsh
@@ -6,4 +6,12 @@ if [ "$USER" = "winnochan" ]; then
     ln -s $ZSH_DIR/config/gitconfig $HOME/.gitconfig
     echo 'link git config'
   fi
+
+  if [ ! -L $HOME/.gitattributes ]; then
+    if [ -e $HOME/.gitattributes ]; then
+      rm -rf $HOME/.gitattributes
+    fi
+    ln -s $ZSH_DIR/config/gitattributes $HOME/.gitattributes
+    echo 'link git attributes'
+  fi
 fi
