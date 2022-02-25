8e9eb5b6bd2cd2a397699bd7fe9b2d82a4d469cc
WinnoChan <winnochan@icloud.com>
2022-02-25 14:06:00 +0800

    update rust mirror

--- plugins/rust.plugin.zsh
+++ plugins/rust.plugin.zsh
@@ -22,10 +22,12 @@ if command -v rustup >/dev/null 2>&1; then
       fi
     fi
 
-    # export RUSTUP_DIST_SERVER=https://mirror.sjtu.edu.cn/rust-static
-    # export RUSTUP_UPDATE_ROOT=https://mirror.sjtu.edu.cn/rust-static/rustup
-    export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static
-    export RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup
+    export RUSTUP_DIST_SERVER=https://mirror.sjtu.edu.cn/rust-static
+    export RUSTUP_UPDATE_ROOT=https://mirror.sjtu.edu.cn/rust-static/rustup
+    # export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static
+    # export RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup
+    # export RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup
+    # export RUSTUP_UPDATE_ROOT=https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
 
     if [ "$RUST_SYSROOT" != "" ]; then
       export RUST_SRC_PATH=$RUST_SYSROOT/lib/rustlib/src/rust/library
