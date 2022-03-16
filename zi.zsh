1787c7c47059fdfb6e9d0f471f7ec5291a1bc3cc
WinnoChan <winnochan@icloud.com>
2022-03-16 17:52:17 +0800

    use zi plugin

--- zi.zsh
+++ zi.zsh
@@ -1,5 +1,3 @@
-autoload -Uz compinit
-
 declare -A ZI
 ZI[HOME_DIR]=$ZSH_DIR/.zi
 ZI[BIN_DIR]=${ZI[HOME_DIR]}/bin
@@ -10,41 +8,38 @@ ZI[SNIPPETS_DIR]=${ZI[CACHE_DIR]}/snippets
 ZI[ZCOMPDUMP_PATH]=$HOME
 ZI[COMPINIT_OPTS]='-C'
 ZI[MUTE_WARNINGS]=1
-ZI[OPTIMIZE_OUT_DISK_ACCESSES]=0
+ZI[OPTIMIZE_OUT_DISK_ACCESSES]=1
 
 source ${ZI[BIN_DIR]}/zi.zsh
 
-zi ice silent wait lucid multisrc='lib/*.zsh' from=gitee.com; zi light mirrors/oh-my-zsh
+zi wait lucid light-mode for multisrc='lib/*.zsh' from=gitee.com mirrors/oh-my-zsh
 
-zi ice silent wait lucid; zi snippet https://gitee.com/mirrors/oh-my-zsh/raw/master/plugins/alias-finder/alias-finder.plugin.zsh
+zi wait lucid for \
+   https://gitee.com/mirrors/oh-my-zsh/raw/master/plugins/alias-finder/alias-finder.plugin.zsh \
+   https://gitee.com/mirrors/oh-my-zsh/raw/master/plugins/extract/extract.plugin.zsh \
+   https://gitee.com/mirrors/oh-my-zsh/raw/master/plugins/fasd/fasd.plugin.zsh \
+   https://gitee.com/mirrors/oh-my-zsh/raw/master/plugins/git/git.plugin.zsh \
+   https://gitee.com/mirrors/oh-my-zsh/raw/master/plugins/last-working-dir/last-working-dir.plugin.zsh \
+   https://gitee.com/winnochan/fzf-fasd/raw/master/fzf-fasd.plugin.zsh
 
-zi ice silent wait lucid; zi snippet https://gitee.com/mirrors/oh-my-zsh/raw/master/plugins/extract/extract.plugin.zsh
-
-zi ice silent wait lucid; zi snippet https://gitee.com/mirrors/oh-my-zsh/raw/master/plugins/fasd/fasd.plugin.zsh
-
-zi ice silent wait lucid; zi snippet https://gitee.com/mirrors/oh-my-zsh/raw/master/plugins/git/git.plugin.zsh
-
-zi ice silent wait lucid; zi snippet https://gitee.com/mirrors/oh-my-zsh/raw/master/plugins/last-working-dir/last-working-dir.plugin.zsh
-
-zi ice silent wait lucid multisrc='shell/*.zsh' from=gitee.com; zi light mirrors/fzf
-
-zi ice silent wait lucid; zi snippet https://gitee.com/winnochan/fzf-fasd/raw/master/fzf-fasd.plugin.zsh
+zi wait lucid light-mode for multisrc='shell/*.zsh' from=gitee.com mirrors/fzf
 
 # blox theme
-zi ice silent wait=! atload=blox_hook__render from=gitee.com pick=blox.zsh src=blox.zsh; zi light winnochan/blox-zsh-theme
+zi silent wait=! light-mode for from=gitee.com src=blox.zsh atload=blox_hook__render winnochan/blox-zsh-theme
+# zi ice silent wait=! atload=blox_hook__render from=gitee.com src=blox.zsh; zi light winnochan/blox-zsh-theme
 
 # 自动推荐历史命令插件
 zi ice silent wait lucid atload=_zsh_autosuggest_start from=gitee.com; zi light winnochan/zsh-autosuggestions
 
 # 大量常用命令的补全插件
 # zi ice silent blockf from=gitee.com; zi light winnochan/zsh-completions
-zi ice silent wait lucid from=gitee.com; zi light winnochan/zsh-completions
+zi wait lucid light-mode for from=gitee.com winnochan/zsh-completions
 
 # 自动括号, 引号匹配插件
-zi ice silent wait lucid from=gitee.com; zi light winnochan/zsh-autopair
+zi wait lucid light-mode for from=gitee.com winnochan/zsh-autopair
 
 # 语法高亮插件
-zi ice silent wait lucid from=gitee.com; zi light winnochan/fast-syntax-highlighting
+zi wait lucid light-mode for from=gitee.com winnochan/fast-syntax-highlighting
 
 autoload -Uz _zi
 (( ${+_comps} )) && _comps[zi]=_zi
