694cfbde712eedf805b51779796ad8f15bc98f68
WinnoChan <winnochan@icloud.com>
2022-03-18 14:08:12 +0800

    blox sync

--- zi.zsh
+++ zi.zsh
@@ -29,9 +29,12 @@ zi silent wait lucid light-mode for \
    from=gitee.com winnochan/fast-syntax-highlighting \
    atload=_zsh_autosuggest_start from=gitee.com winnochan/zsh-autosuggestions
 
-# 主题配置(blox theme)
-zi silent wait=! lucid light-mode for \
-   atload=blox_hook__render src=blox.zsh from=gitee.com winnochan/blox-zsh-theme
+# 主题配置(blox theme), 异步
+# zi silent wait=! lucid light-mode for \
+#    atload=blox_hook__render src=blox.zsh from=gitee.com winnochan/blox-zsh-theme
+
+# 主题配置(blox theme), 同步
+zi silent lucid light-mode for src=blox.zsh from=gitee.com winnochan/blox-zsh-theme
 
 autoload -Uz _zi
 (( ${+_comps} )) && _comps[zi]=_zi
