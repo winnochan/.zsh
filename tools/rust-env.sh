c089cb62d759fb1fd8db68d8e2b84a9b927fe24d
WinnoChan <winnochan@icloud.com>
2022-07-22 15:38:57 +0800

    update cargo config

--- tools/rust-env.sh
+++ tools/rust-env.sh
@@ -5,14 +5,11 @@ fi
 
 if command -v rustup >/dev/null 2>&1; then
   rustup component add llvm-tools-preview rls rust-analysis rust-src rustfmt clippy
-  rustup toolchain add nightly
 elif [ -f $ZSH_DIR/plugins/rust.plugin.zsh ]; then
   source $ZSH_DIR/plugins/rust.plugin.zsh
   rustup component add llvm-tools-preview rls rust-analysis rust-src rustfmt clippy
-  rustup toolchain add nightly
 fi
 
 if command -v cargo >/dev/null 2>&1; then
   cargo install cargo-edit cargo-audit cargo-outdated
-  cargo +nightly install racer
 fi
