c7c3a99cdfca73327de7ef5bfe14d2c7ed53c03a
WinnoChan <winnochan@icloud.com>
2023-02-15 12:08:11 +0800

    update rust mirror

--- plugins/rust.plugin.zsh
+++ plugins/rust.plugin.zsh
@@ -22,10 +22,10 @@ if command -v rustup >/dev/null 2>&1; then
       fi
     fi
 
-    export RUSTUP_DIST_SERVER=https://mirror.sjtu.edu.cn/rust-static
-    export RUSTUP_UPDATE_ROOT=https://mirror.sjtu.edu.cn/rust-static/rustup
-    # export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static
-    # export RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup
+    # export RUSTUP_DIST_SERVER=https://mirror.sjtu.edu.cn/rust-static
+    # export RUSTUP_UPDATE_ROOT=https://mirror.sjtu.edu.cn/rust-static/rustup
+    export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static
+    export RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup
     # export RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup
     # export RUSTUP_UPDATE_ROOT=https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
 
