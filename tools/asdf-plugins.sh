b6edbd33a186f291fbc58fbe64dcbf16f37dc19e
WinnoChan <winnochan@icloud.com>
2023-09-05 16:23:42 +0800

    update asdf plugin url

--- tools/asdf-plugins.sh
+++ tools/asdf-plugins.sh
@@ -1,11 +1,11 @@
 brew install readline xz gnupg
 
-asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
+asdf plugin-add golang https://gitee.com/winnochan/asdf-golang.git
 asdf install golang $(asdf latest golang)
 
-asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf plugin-add nodejs https://gitee.com/winnochan/asdf-nodejs.git
 bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
 asdf install nodejs $(asdf latest nodejs 14)
 
-asdf plugin-add python
+asdf plugin-add python https://gitee.com/winnochan/asdf-python.git
 asdf install python $(asdf latest python 3)
